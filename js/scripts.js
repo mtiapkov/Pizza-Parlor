7388f017bd7e788640240d227575240f28ca7582
Mark Tiapkov <mark.tiapkov@gmail.com>
2020-08-28 11:27:43 -0700

    create add toppings and order pizza methods, adjust ui logic

--- js/scripts.js
+++ js/scripts.js
@@ -6,20 +6,32 @@ function Pizza(size, toppings) {
   this.toppings = [toppings]
 }
 
-//Adding Toppings Logic
+//Adding Toppings Method
+Pizza.prototype.addingToppings = function (toppings) {
+  this.toppings.push(toppings);
+}
+
+//Ordered Pizza Method
+function orderedPizza(pizza) {
+$(".pizzaOrder").append(pizzaSize + "<br>")
+$(".pizzaOrder").append(pizzaToppings + "<br>")
+q}
 
 
 
-
+const pizza = new Pizza();  
 //User Interface Logic
 $(document).ready(function(event) {
+  $("form#selectPizza").submmit(function(event){
   event.preventDefault();
 
-  $(".pizzaOrder").show();
-  const pizzaSize = $("#selectSize").val();
-  $(".pizzaOrder").append(pizzaSize + "<br>")
-  $("input:checkbox[name=toppings]:checked").each(function(){
-    const pizzaToppings = $(this).val();
-    $(".pizzaOrder").append(pizzaToppings + "<br>");
-  })
-})
+    $(".pizzaOrder").show();
+    const pizzaSize = $("#selectSize").val();
+    $(".pizzaOrder").append(pizzaSize + "<br>")
+    $("input:checkbox[name=toppings]:checked").each(function(){
+      const pizzaToppings = $(this).val();
+  });
+
+  });
+
+});
